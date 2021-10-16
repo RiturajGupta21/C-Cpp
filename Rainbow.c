eff99d2af39511d55ef28f43db233cfc9b0aefb4
Rituraj Gupta <rituraj.iter@gmail.com>
2021-10-16 21:44:05 +0530

    Create Rainbow.c

---
+++ Rainbow.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<graphics.h>
+void main()
+{
+	int gd=DETECT,gm,i,x,y;
+	initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+	x=getmaxx()/2;
+	y=getmaxy()/2;
+
+	for(i=30;i<200;i++)
+	{
+		delay(100);
+		setcolor(i/10);
+		arc(x,y,0,180,i-10);
+	}
+getch();
+	closegraph();
+}
